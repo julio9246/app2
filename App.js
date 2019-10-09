a29d36cd8f17c9f33744ff48d1d1b6184a805a19
Julio Cesar <julio9246@hotmail.com>
2019-10-08 21:52:56 -0300

    fazendo alteraçao layout margin

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View>
-        <Text style={Estilos.estiloTexto}>Meu App 2</Text>
+        <Text style={Estilos.estiloTexto}>Meu App 2 Hoje</Text>
     </View>
 
    
@@ -23,8 +23,11 @@ const styles = StyleSheet.create({
 const Estilos = {
   estiloTexto : { 
     fontSize: 30,
-    backgroundColor: '#08509B', 
-    width: 360,
-    top: 23
+    backgroundColor: '#015A91',  
+    padding: 10,
+    marginTop: 24,
+    /// marginHorizontal: 30
+    // paddingTop: 10
+    // paddingLeft: 10
   }
 };
